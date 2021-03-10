d106b592c217c5466c5d6881e1a4bd30f4024d4c
Huynh Quoc Bao <44991601+quocbao0802@users.noreply.github.com>
2021-03-10 11:45:36 +0700

    Spring initializr

---
+++ src/main/java/com/spring/onistore/OnistoreApplication.java
@@ -0,0 +1,13 @@
+package com.spring.onistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnistoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/onistore/OnistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.onistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
