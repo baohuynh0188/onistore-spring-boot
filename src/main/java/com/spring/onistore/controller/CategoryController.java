2428cc91252d58a20bf05d90c8a593cec685aa6c
azjustin1 <azjustin3@gmail.com>
2021-03-28 17:09:11 +0700

    fix Product and Category

--- src/main/java/com/spring/onistore/controller/CategoryController.java
+++ src/main/java/com/spring/onistore/controller/CategoryController.java
@@ -16,61 +16,57 @@ import static com.spring.onistore.util.Slugify.toSlug;
 @RestController
 @RequestMapping("api/v2")
 public class CategoryController {
-@Autowired
-private CategoryRepository categoryRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
-@GetMapping("categories")
-public List<Category> getAllCategories() {
-return categoryRepository.findAll();
-}
+	@GetMapping("categories")
+	public List<Category> getAllCategories() {
+		return categoryRepository.findAll();
+	}
 
-// // Get products by category name
-// @GetMapping("categories/{category_name}")
-// public ResponseEntity<List<Category>>
-getCategoryByName(@PathVariable(value = "category_name") String categoryName)
-throws ResourceNotFoundException {
-// List<Category> categories =
-categoryRepository.findByCategoryName(categoryName);
-// return ResponseEntity.ok().body(categories);
-// }
+	// Get products by category name
+	@GetMapping("categories/{category_name}")
+	public ResponseEntity<List<Category>> getCategoryByName(@PathVariable(value = "category_name") String categoryName)
+			throws ResourceNotFoundException {
+		List<Category> categories = categoryRepository.findByCategoryName(categoryName);
+		return ResponseEntity.ok().body(categories);
+	}
 
-// Get products by category slug
-@GetMapping("categories/{category_slug}")
-public ResponseEntity<List<Category>> getCategoryBySlug(@PathVariable(value =
-"category_slug") String categorySlug) throws ResourceNotFoundException {
-List<Category> categories =
-categoryRepository.findByCategorySlug(categorySlug);
-return ResponseEntity.ok().body(categories);
-}
+	// Get products by category slug
+	@GetMapping("categories/{category_slug}")
+	public ResponseEntity<List<Category>> getCategoryBySlug(@PathVariable(value = "category_slug") String categorySlug)
+			throws ResourceNotFoundException {
+		List<Category> categories = categoryRepository.findByCategorySlug(categorySlug);
+		return ResponseEntity.ok().body(categories);
+	}
 
-@PostMapping("categories/add")
-public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-category.setCategorySlug(toSlug(category.getCategoryName()));
-Category saveCategory = categoryRepository.save(category);
-return ResponseEntity.ok().body(saveCategory);
-}
+	@PostMapping("categories/add")
+	public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+		category.setCategorySlug(toSlug(category.getCategoryName()));
+		Category saveCategory = categoryRepository.save(category);
+		return ResponseEntity.ok().body(saveCategory);
+	}
 
-@PutMapping("categories/{id}")
-public ResponseEntity<Category> updateCategory(@PathVariable(value = "id")
-Long categoryId, @RequestBody Category categoryDetail) throws
-ResourceNotFoundException {
-Category category = categoryRepository.findById(categoryId).orElseThrow(() ->
-new ResourceNotFoundException("Category " + categoryId + " is not exist"));
-category.setCategoryName(categoryDetail.getCategoryName());
-category.setCategoryDescription(categoryDetail.getCategoryDescription());
-category.setCategorySlug(toSlug(categoryDetail.getCategoryName()));
-final Category categoryUpdated = categoryRepository.save(category);
-return ResponseEntity.ok().body(categoryUpdated);
-}
+	@PutMapping("categories/{id}")
+	public ResponseEntity<Category> updateCategory(@PathVariable(value = "id") Long categoryId,
+			@RequestBody Category categoryDetail) throws ResourceNotFoundException {
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Category " + categoryId + " is not exist"));
+		category.setCategoryName(categoryDetail.getCategoryName());
+		category.setCategoryDescription(categoryDetail.getCategoryDescription());
+		category.setCategorySlug(toSlug(categoryDetail.getCategoryName()));
+		final Category categoryUpdated = categoryRepository.save(category);
+		return ResponseEntity.ok().body(categoryUpdated);
+	}
 
-@DeleteMapping("categories/{id}")
-public Map<String, Boolean> deleteCategory(@PathVariable(value = "id") Long
-categoryId) throws ResourceNotFoundException {
-Category category = categoryRepository.findById(categoryId).orElseThrow(() ->
-new ResourceNotFoundException("Category " + categoryId + " is not exist"));
-categoryRepository.delete(category);
-Map<String, Boolean> response = new HashMap<>();
-response.put("deleted", Boolean.TRUE);
-return response;
-}
+	@DeleteMapping("categories/{id}")
+	public Map<String, Boolean> deleteCategory(@PathVariable(value = "id") Long categoryId)
+			throws ResourceNotFoundException {
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Category " + categoryId + " is not exist"));
+		categoryRepository.delete(category);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
 }
--- src/main/java/com/spring/onistore/repository/ProductRepository.java
+++ src/main/java/com/spring/onistore/repository/ProductRepository.java
@@ -1,11 +1,12 @@
-// package com.spring.onistore.repository;
+package com.spring.onistore.repository;
 
-// import com.spring.onistore.model.Product;
-// import org.springframework.data.jpa.repository.JpaRepository;
+import com.spring.onistore.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import java.util.List;
+import java.util.List;
 
-// public interface ProductRepository extends JpaRepository<Product, Long> {
-// List<Product> findByProductName(String productName);
-// List<Product> findByProductSlug(String productSlug);
-// }
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	List<Product> findByProductName(String productName);
+
+	List<Product> findByProductSlug(String productSlug);
+}
